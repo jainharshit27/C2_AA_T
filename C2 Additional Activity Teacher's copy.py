38af1a47a55554f42e9ed4441c973cb31021d04f
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-06-09 20:09:53 +0530

    Add files via upload

---
+++ C2 Additional Activity Teacher's copy.py
@@ -0,0 +1,36 @@
+
+
+
+# Activity 1
+print(7+8)
+
+
+
+
+
+
+
+# Activity 2
+# This is print() function printing ABC
+print("ABC")
+
+
+# This is print() function printing 123
+print(123)
+
+
+# This is print() function printing 25 + 50
+print(25 + 50)
+
+
+# This is print() function printing 123
+print(25, 50)
+
+
+# This is print() function printing cde
+print("abc" + "cde")
+
+
+# This is print() function printing cde
+print("abc", "cde")
+
